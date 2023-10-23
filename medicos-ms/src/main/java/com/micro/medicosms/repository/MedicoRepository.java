4bec78927323440f29f0b13a2438bfa8c4e84899
ntheus <matheusoliveira.7313@gmail.com>
2023-10-23 12:44:59 -0300

    buscas personalizadas criadas

--- medicos-ms/src/main/java/com/micro/medicosms/repository/MedicoRepository.java
+++ medicos-ms/src/main/java/com/micro/medicosms/repository/MedicoRepository.java
@@ -1,9 +1,18 @@
 package com.micro.medicosms.repository;
 
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.micro.medicosms.model.Medico;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+
+    Optional<Medico> findByDadosCadastraisCrm(String crm);// TODO adicionar status em discussao se devo ou nao
+
+    Optional<Medico> findByIdAndStatus(Long id, Boolean status);
+
+    Page<Medico> findAllByStatus(Boolean status, Pageable pageable);
 
 }

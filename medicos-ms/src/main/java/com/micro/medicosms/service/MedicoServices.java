cdc3c6f973eef858a40eb1a271b3746f93c834fa
ntheus <matheusoliveira.7313@gmail.com>
2023-10-23 14:07:09 -0300

    metodos criados, incompletos

--- medicos-ms/src/main/java/com/micro/medicosms/service/MedicoServices.java
+++ medicos-ms/src/main/java/com/micro/medicosms/service/MedicoServices.java
@@ -1,5 +1,35 @@
 package com.micro.medicosms.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.micro.medicosms.form.MedicoForm;
+import com.micro.medicosms.model.Medico;
+import com.micro.medicosms.repository.MedicoRepository;
+
 public class MedicoServices {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    public Medico cadastraMedico(MedicoForm medico) {
+        return null;
+    }
+
+    public Medico editaMedico(Long id, MedicoForm medico) {
+        return null;
+    }
+
+    public Medico encontraUmMedico(Long id) {
+        return null;
+    }
+
+    public void apagaMedico(Long id) {
+    }
+
+    public Page<Medico> listaMedicos(Pageable pageable) {
+        return medicoRepository.findAllByStatus(true, pageable);
+    }
 
 }

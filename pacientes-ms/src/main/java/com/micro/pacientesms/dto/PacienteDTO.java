c6e44bde84a4e7d79e09d5c27f5f2056ab0977df
ntheus <matheusoliveira.7313@gmail.com>
2023-10-23 01:15:40 -0300

    nao requiridos removidos

--- pacientes-ms/src/main/java/com/micro/pacientesms/dto/PacienteDTO.java
+++ pacientes-ms/src/main/java/com/micro/pacientesms/dto/PacienteDTO.java
@@ -13,14 +13,12 @@ public class PacienteDTO {
 
     private Long NumeroDePaciente;
     private String email;
-    private String telefone;
     private String nome;
     private String cpf;
 
     public PacienteDTO(Paciente paciente) {
         this.NumeroDePaciente = paciente.getId();
         this.email = paciente.getEmail().toString();
-        this.telefone = paciente.getTelefone().toString();
         this.nome = paciente.getNome();
         this.cpf = paciente.getCPF().toString();
     }

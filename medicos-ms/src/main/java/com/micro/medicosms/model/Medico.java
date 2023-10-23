f72ad8df20ac99734dacf6cef4d7f9416b9f690d
ntheus <matheusoliveira.7313@gmail.com>
2023-10-23 12:46:51 -0300

    delegacao de getters

--- medicos-ms/src/main/java/com/micro/medicosms/model/Medico.java
+++ medicos-ms/src/main/java/com/micro/medicosms/model/Medico.java
@@ -6,6 +6,9 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.micro.medicosms.model.Campos.DadosCadastrais;
+import com.micro.medicosms.model.Campos.Email;
+import com.micro.medicosms.model.Campos.Especialidade;
+import com.micro.medicosms.model.Campos.Telefone;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -41,5 +44,25 @@ public class Medico {
     private LocalTime created;
     @UpdateTimestamp
     private LocalTime update;
+
+    public Email getEmail() {
+        return this.getDadosCadastrais().getEmail();
+    }
+
+    public String getNome() {
+        return this.getDadosCadastrais().getNome();
+    }
+
+    public Telefone getTelefone() {
+        return this.getDadosCadastrais().getTelefone();
+    }
+
+    public String getCRM() {
+        return this.getDadosCadastrais().getCrm();
+    }
+
+    public Especialidade getEspecialidade() {
+        return this.getDadosCadastrais().getEspecialidade();
+    }
 
 }

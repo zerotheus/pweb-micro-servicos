1286870e38cca2a0f9eb9af7b41e4940f6dc4aeb
ntheus <matheusoliveira.7313@gmail.com>
2023-10-23 14:05:57 -0300

    adapter inciado

---
+++ medicos-ms/src/main/java/com/micro/medicosms/adapter/DadosCadastraisAdapter.java
@@ -0,0 +1,9 @@
+package com.micro.medicosms.adapter;
+
+import com.micro.medicosms.form.DadosCadastraisForm;
+
+public class DadosCadastraisAdapter {
+
+    private DadosCadastraisForm dadosCadastraisForm;
+
+}

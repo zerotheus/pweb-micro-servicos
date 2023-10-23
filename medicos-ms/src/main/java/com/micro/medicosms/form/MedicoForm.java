dd6f24597ec4ac1c952f880bcf5ea9a9df2dbf0b
ntheus <matheusoliveira.7313@gmail.com>
2023-10-23 14:06:44 -0300

    form criados

--- medicos-ms/src/main/java/com/micro/medicosms/form/MedicoForm.java
+++ medicos-ms/src/main/java/com/micro/medicosms/form/MedicoForm.java
@@ -1,5 +1,12 @@
 package com.micro.medicosms.form;
 
+import jakarta.validation.Valid;
+
 public class MedicoForm {
+
+    @Valid
+    private DadosCadastraisForm dadosCadastrais;
+    @Valid
+    private EnderecoForm endereco;
 
 }

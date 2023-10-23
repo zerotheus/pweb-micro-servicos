89250ff78c9fedf42f541bea6ddc0aa2347e6668
ntheus <matheusoliveira.7313@gmail.com>
2023-10-23 12:46:30 -0300

    conversao de medico para seu dto

--- medicos-ms/src/main/java/com/micro/medicosms/dto/MedicoDto.java
+++ medicos-ms/src/main/java/com/micro/medicosms/dto/MedicoDto.java
@@ -1,5 +1,26 @@
 package com.micro.medicosms.dto;
 
+import com.micro.medicosms.model.Medico;
+import com.micro.medicosms.model.Campos.Especialidade;
+
+import lombok.Getter;
+
+@Getter
 public class MedicoDto {
+
+    private Long NumeroIndetificador;
+    private String Nome;
+    private String Email;
+    private String CRM;
+    private Especialidade especialidade;
+
+    public MedicoDto(Medico medico) {
+        this.NumeroIndetificador = medico.getId();
+        this.Nome = medico.getNome();
+        this.Email = medico.getEmail().toString();
+        this.CRM = medico.getCRM();
+        this.especialidade = medico.getDadosCadastrais().getEspecialidade();
+
+    }
 
 }

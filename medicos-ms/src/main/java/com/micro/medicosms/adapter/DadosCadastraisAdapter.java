a1087f8dfd52378c4696fb55e936a0b7a8a7591a
ntheus <matheusoliveira.7313@gmail.com>
2023-10-24 02:11:10 -0300

    adapter de dados cadastrais

--- medicos-ms/src/main/java/com/micro/medicosms/adapter/DadosCadastraisAdapter.java
+++ medicos-ms/src/main/java/com/micro/medicosms/adapter/DadosCadastraisAdapter.java
@@ -1,9 +1,26 @@
 package com.micro.medicosms.adapter;
 
 import com.micro.medicosms.form.DadosCadastraisForm;
+import com.micro.medicosms.model.Campos.DadosCadastrais;
+import com.micro.medicosms.model.Campos.Email;
+import com.micro.medicosms.model.Campos.Telefone;
 
 public class DadosCadastraisAdapter {
 
     private DadosCadastraisForm dadosCadastraisForm;
+
+    DadosCadastraisAdapter(DadosCadastraisForm dadosCadastraisForm) {
+        this.dadosCadastraisForm = dadosCadastraisForm;
+    }
+
+    public DadosCadastrais converte() {
+        DadosCadastrais dadosCadastrais = new DadosCadastrais();
+        dadosCadastrais.setNome(dadosCadastraisForm.getNome());
+        dadosCadastrais.setCrm(dadosCadastraisForm.getCrm());
+        dadosCadastrais.setEmail(new Email(dadosCadastraisForm.getEmail()));
+        dadosCadastrais.setEspecialidade(dadosCadastraisForm.getEspecialidade());
+        dadosCadastrais.setTelefone(new Telefone(dadosCadastraisForm.getTelefone()));
+        return dadosCadastrais;
+    }
 
 }

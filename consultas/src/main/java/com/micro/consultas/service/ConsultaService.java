28bd658567e58fd8fa7764b8a5e4296c05e7be7f
ntheus <matheusoliveira.7313@gmail.com>
2023-10-27 03:28:11 -0300

    tratamento de resposta e de envio de mensagens

--- consultas/src/main/java/com/micro/consultas/service/ConsultaService.java
+++ consultas/src/main/java/com/micro/consultas/service/ConsultaService.java
@@ -1,27 +1,35 @@
 package com.micro.consultas.service;
 
+import java.util.Optional;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.micro.consultas.Regra.RegrasDeMarcacaoDeConsulta;
 import com.micro.consultas.adapter.ConsultaAdapter;
+import com.micro.consultas.amqp.MensagemAMQP;
 import com.micro.consultas.form.ConsultaForm;
 import com.micro.consultas.model.Consulta;
 import com.micro.consultas.repository.ConsultaRepository;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class ConsultaService {
 
     @Autowired
     private ConsultaRepository consultaRepository;
     @Autowired
     private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private final MutexConsultas mutexConsultas;
 
     public Consulta agendaConsulta(ConsultaForm consultaForm) {
         regrasDeMarcacao(consultaForm);
-        enviaMensagemParaPaciente(consultaForm);
-        return adaptaConsulta(consultaForm);
+        envieMensagens(consultaForm);
+        return consultaRepository.save(adaptaConsulta(consultaForm));
     }
 
     public void regrasDeMarcacao(ConsultaForm consultaForm) {
@@ -32,12 +40,33 @@ public class ConsultaService {
         return new ConsultaAdapter(consultaForm).converte();
     }
 
-    public void enviaMensagemParaMedico() {
+    public void envieMensagens(ConsultaForm consultaForm) {
+        enviaMensagemParaMedico(consultaForm);
+        enviaMensagemParaPaciente(consultaForm);
+    }
+
+    private void enviaMensagemParaMedico(ConsultaForm consultaForm) {
 
     }
 
-    public void enviaMensagemParaPaciente(ConsultaForm consultaForm) {
+    private void enviaMensagemParaPaciente(ConsultaForm consultaForm) {
         rabbitTemplate.convertAndSend("Pacientes", consultaForm.getPacienteId());
+    }
+
+    public void trataResposta(ResponseHandler responseHandler, MensagemAMQP message) {
+        mutexConsultas.bloqueia(message.getConsultaId());
+        Optional<Consulta> consulta = consultaRepository.findById(message.getConsultaId());
+        if (consulta.isEmpty()) {
+            return;
+        }
+        try {
+            responseHandler.alterarEstado(consulta.get(), message);
+            consultaRepository.save(consulta.get());
+        } catch (Exception e) {
+            consultaRepository.deleteById(message.getConsultaId());
+        } finally {
+            mutexConsultas.desbloqueia(message.getConsultaId());
+        }
     }
 
 }

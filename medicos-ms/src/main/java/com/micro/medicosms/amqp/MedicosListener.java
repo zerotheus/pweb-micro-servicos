e99fe3d0450b6e04bb735d68ce8e7cc80079b2c6
ntheus <matheusoliveira.7313@gmail.com>
2023-10-27 17:05:15 -0300

    recebendo mensagens da fila

--- medicos-ms/src/main/java/com/micro/medicosms/amqp/MedicosListener.java
+++ medicos-ms/src/main/java/com/micro/medicosms/amqp/MedicosListener.java
@@ -5,15 +5,32 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.micro.medicosms.service.MedicoServices;
+
 @Component
 public class MedicosListener {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private MedicoServices medicoServices;
 
     @RabbitListener(queues = "Medicos")
-    public void EsperaPedidos(Long id) {
-        System.out.println("id: " + id);// TODO decidir a forma de tratar erro
+    public void EsperaPedidos(MensagemAMQP message) {
+        System.out.println(message);
+        try {
+            medicoServices.encontraUmMedico(message.getRequiredId());
+            message.setExiste(true);
+            rabbitTemplate.convertAndSend("Medicos Response", message);
+        } catch (Exception e) {
+            rabbitTemplate.convertAndSend("Medicos Response", message);
+        }
+    }
+
+    @RabbitListener(queues = "Liste todos Medicos")
+    public void EsperaSolicitacoesDeListagem(ListagemMessage listagem) {
+        listagem.setSenderId(0);
+        medicoServices.listaMedicos(listagem.getLista());
     }
 
 }

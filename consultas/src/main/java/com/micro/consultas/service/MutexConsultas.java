47729cf4ed5158286d206d2ec070cb7c06fe32a9
ntheus <matheusoliveira.7313@gmail.com>
2023-10-27 03:25:05 -0300

    controle de concorrencia

---
+++ consultas/src/main/java/com/micro/consultas/service/MutexConsultas.java
@@ -0,0 +1,26 @@
+package com.micro.consultas.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MutexConsultas {
+
+    private Map<Long, Lock> consultasEmAnalise = new HashMap<Long, Lock>();
+
+    public void bloqueia(Long consultaId) {
+        Lock lock = new ReentrantLock();
+        consultasEmAnalise.put(consultaId, lock);
+        lock.lock();
+    }
+
+    public void desbloqueia(Long consultaId) {
+        Lock lock = consultasEmAnalise.get(consultaId);
+        if (lock != null) {
+            lock.unlock();
+        }
+        consultasEmAnalise.remove(consultaId);
+    }
+
+}

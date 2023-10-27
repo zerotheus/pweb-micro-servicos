4093605fdee63306c0784bbd9519c81908c8b52d
ntheus <matheusoliveira.7313@gmail.com>
2023-10-27 03:32:19 -0300

    comunicacao com servico

--- consultas/src/main/java/com/micro/consultas/amqp/ConsultaResponseListener.java
+++ consultas/src/main/java/com/micro/consultas/amqp/ConsultaResponseListener.java
@@ -1,20 +1,26 @@
 package com.micro.consultas.amqp;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.micro.consultas.service.ConsultaService;
 
 @Component
 public class ConsultaResponseListener {
 
+    @Autowired
+    private ConsultaService consultaService;
+
     @RabbitListener(queues = "Medicos Response")
-    public void escutaRespostaDeMedico(boolean existe) {
+    public void escutaRespostaDeMedico(MensagemAMQP mensagemAMQP) {
         System.out.println("Medico respondeu");
     }
 
     @RabbitListener(queues = "Pacientes Response")
-    public void escutaRespostaDePaciente(boolean existe) {
+    public void escutaRespostaDePaciente(MensagemAMQP mensagemAMQP) {
         System.out.println("Paciente respondeu");
-        System.out.println(existe);
+        System.out.println(mensagemAMQP.isExiste());
     }
 
 }

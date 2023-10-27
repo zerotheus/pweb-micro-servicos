04080b4e8b396fce5b2a3807f09e20d320c6f8a5
ntheus <matheusoliveira.7313@gmail.com>
2023-10-27 03:29:06 -0300

    metodo que devolve como map nao testado

--- consultas/src/main/java/com/micro/consultas/repository/ConsultaRepository.java
+++ consultas/src/main/java/com/micro/consultas/repository/ConsultaRepository.java
@@ -1,7 +1,9 @@
 package com.micro.consultas.repository;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,9 +18,18 @@ public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     @Query(nativeQuery = true, value = "Select * from consultas where horario > ?2 and fk_paciente_id = ?1")
     public List<Consulta> jaPossuiConsultaAgendada(@Param(value = "id") Long id, LocalDateTime diaMarcado);
 
-    @Query(nativeQuery = true, value = "SELECT fk_medico_id FROM consultas WHERE horario >= :horaMarcada AND horario <= :termino and estado != 'Cancelada'")
+    @Query(nativeQuery = true, value = "SELECT DISTINCT fk_medico_id FROM consultas WHERE horario >= :horaMarcada AND horario <= :termino and estado != 'Cancelada'")
     public List<Long> medicosIndisponiveis(@Param("horaMarcada") LocalDateTime horaMarcada,
             @Param("termino") LocalDateTime termino);
+
+    default HashMap<Long, Long> medicosIndisponiveisAsMap(LocalDateTime horaMarcada, LocalDateTime termino) {
+        List<Long> lista = medicosIndisponiveis(horaMarcada, termino);
+        HashMap<Long, Long> hashmap = new HashMap<Long, Long>();
+        for (int i = 0; i < lista.size(); i++) {
+            hashmap.put(lista.get(i), lista.get(i));
+        }
+        return hashmap;
+    }
 
     @Query(nativeQuery = true, value = "SELECT * FROM consultas WHERE fk_medico_id = :medico and horario >= :horaMarcada AND horario <= :termino and estado != 'Cancelada'")
     public List<Consulta> medicoSelecionadoEstaDisponivel(@Param("medico") Long id,

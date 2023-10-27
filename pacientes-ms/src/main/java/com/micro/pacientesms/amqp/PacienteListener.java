d21327e296426c12d51f11d53b927b95ad0fd7ff
ntheus <matheusoliveira.7313@gmail.com>
2023-10-26 23:28:20 -0300

    mudanca de tratamento de mensageria

--- pacientes-ms/src/main/java/com/micro/pacientesms/amqp/PacienteListener.java
+++ pacientes-ms/src/main/java/com/micro/pacientesms/amqp/PacienteListener.java
@@ -5,15 +5,31 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.micro.pacientesms.Service.PacienteServices;
+
 @Component
 public class PacienteListener {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private PacienteServices pacienteServices;
+
     @RabbitListener(queues = "Pacientes")
     public void EsperaPedidos(Long id) {
-        System.out.println("id: " + id);
+        System.out.println("id: " + id);// TODO decidir a forma de tratar erro
+        try {
+            pacienteServices.encontraPaciente(id);
+            rabbitTemplate.convertAndSend("Pacientes Response", true);
+        } catch (Exception e) {
+            rabbitTemplate.convertAndSend("Pacientes Response", false);
+        }
+    }
+
+    @RabbitListener(queues = "Liste todos pacientes")
+    public void EsperaSolicitacoesDeListagem() {
+        //TODO responder
     }
 
 }

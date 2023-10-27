16cc90fb1bfbf028291f7fe20b733f82cb70deba
ntheus <matheusoliveira.7313@gmail.com>
2023-10-27 03:29:54 -0300

    imports removidos

--- consultas/src/main/java/com/micro/consultas/model/Consulta.java
+++ consultas/src/main/java/com/micro/consultas/model/Consulta.java
@@ -15,8 +15,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;

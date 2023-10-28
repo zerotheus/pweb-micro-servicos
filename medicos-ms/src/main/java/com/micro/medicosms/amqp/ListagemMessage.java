5a0906eea24dc6243e373470cf631bd197614dc3
ntheus <matheusoliveira.7313@gmail.com>
2023-10-28 01:02:38 -0300

    ajuste no atributo da mensafem

--- medicos-ms/src/main/java/com/micro/medicosms/amqp/ListagemMessage.java
+++ medicos-ms/src/main/java/com/micro/medicosms/amqp/ListagemMessage.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ListagemMessage {
 
     private int senderId;
-    private Long messageId;
+    private Long consultaId;
     private List<Long> lista;
 
 }

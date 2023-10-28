ed16746e841e75b3a2da2d0df1410a3c732c672a
ntheus <matheusoliveira.7313@gmail.com>
2023-10-28 17:42:20 -0300

    cancela consulta

--- consultas/src/main/java/com/micro/consultas/controller/ConsultaController.java
+++ consultas/src/main/java/com/micro/consultas/controller/ConsultaController.java
@@ -1,14 +1,19 @@
 package com.micro.consultas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.micro.consultas.dto.ConsultaDto;
+import com.micro.consultas.form.CancelamentoForm;
 import com.micro.consultas.form.ConsultaForm;
 import com.micro.consultas.service.ConsultaService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/Consulta")
@@ -18,9 +23,14 @@ public class ConsultaController {
     private ConsultaService consultaService;
 
     @PostMapping
-    public ConsultaDto agendar(@RequestBody ConsultaForm consulta) {
+    public ResponseEntity<ConsultaDto> agendar(@RequestBody ConsultaForm consulta) {
         System.out.println(consulta);
-        return new ConsultaDto(consultaService.agendaConsulta(consulta));
+        return ResponseEntity.ok().body(new ConsultaDto(consultaService.agendaConsulta(consulta)));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ConsultaDto> cancelar(@Valid @RequestBody CancelamentoForm cancelamento) throws Exception {
+        return ResponseEntity.ok().body(new ConsultaDto(consultaService.cancelaConsulta(cancelamento)));
     }
 
 }

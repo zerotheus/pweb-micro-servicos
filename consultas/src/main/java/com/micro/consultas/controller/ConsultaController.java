4620beb047f58c0378e2d40c1d75e44abc33ca5d
ntheus <matheusoliveira.7313@gmail.com>
2023-10-28 16:08:04 -0300

    post renomeado

--- consultas/src/main/java/com/micro/consultas/controller/ConsultaController.java
+++ consultas/src/main/java/com/micro/consultas/controller/ConsultaController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.micro.consultas.Regra.RegrasDeMarcacaoDeConsulta;
 import com.micro.consultas.dto.ConsultaDto;
 import com.micro.consultas.form.ConsultaForm;
 import com.micro.consultas.service.ConsultaService;
@@ -19,7 +18,7 @@ public class ConsultaController {
     private ConsultaService consultaService;
 
     @PostMapping
-    public ConsultaDto testeDePost(@RequestBody ConsultaForm consulta) {
+    public ConsultaDto agendar(@RequestBody ConsultaForm consulta) {
         System.out.println(consulta);
         return new ConsultaDto(consultaService.agendaConsulta(consulta));
     }
